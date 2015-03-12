de4822e180e6cb2e02eebe87ce21a55785a9978b
agulowaty <a.gulowaty@gmail.com>
2015-03-12 20:48:17 +0000

    Problems 57 && 71

---
+++ elementary.clj
@@ -0,0 +1,10 @@
+; http://www.4clojure.com/problem/57
+(=  [5 4 3 2 1] ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5))
+
+
+; http://www.4clojure.com/problem/71
+
+(= (last (sort (rest (reverse [2 5 4 1 3 6]))))
+   (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (last))
+   5)
+
