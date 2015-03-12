ef0c9ca9d1b2af1d1f5367f97b012ad2f0f5ec99
agulowaty <a.gulowaty@gmail.com>
2015-03-12 21:47:09 +0000

    Another problem solved

--- elementary.clj
+++ elementary.clj
@@ -3,8 +3,9 @@
 
 
 ; http://www.4clojure.com/problem/71
-
 (= (last (sort (rest (reverse [2 5 4 1 3 6]))))
    (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (last))
    5)
 
+; http://www.4clojure.com/problem/134
+(true? ((fn [k m] (and (contains? m k) (nil? (m k)))) :a {:a nil :b 2}))
