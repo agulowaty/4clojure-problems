f755170c00675b4e8538406fcc2ad19e7df5991e
agulowaty <a.gulowaty@gmail.com>
2015-03-15 20:43:46 +0000

    Problem 72 added

--- elementary.clj
+++ elementary.clj
@@ -9,3 +9,9 @@
 
 ; http://www.4clojure.com/problem/134
 (true? ((fn [k m] (and (contains? m k) (nil? (m k)))) :a {:a nil :b 2}))
+
+; http://www.4clojure.com/problem/72
+(= (apply + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+   (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (apply +))
+   11)
+
